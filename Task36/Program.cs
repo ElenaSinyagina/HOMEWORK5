835d944535f61d21d14e429794d1eec7d0493126
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-08-26 17:01:24 +0400

    Task

---
+++ Task36/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] createArray(int length)
+{
+    var random = new Random();
+
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(-10, 10);
+
+    return array;
+}
+int sum(int[] array)
+{
+    var result = 0;
+    for (long i = 1; i < array.Length; i = i + 2)
+        result += array[i];
+    return result;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+
+    for (var i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine()!);
+var array = createArray(length);
+printArray(array);
+Console.WriteLine();
+Console.WriteLine("Cумма элементов на нечётных позициях:");
+Console.WriteLine(sum(array));
---
+++ Task38/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между 
+// максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+Console.WriteLine("Введите размер массива");
+int length = int.Parse(Console.ReadLine()!);
+
+double[] createArray()
+{
+    Random random = new Random();
+
+    double[] result = new double[length];
+
+    for (long i = 0; i < result.Length; i++)
+    {
+        result[i] = random.Next(-10, 10);
+    }
+    return result;
+}
+void printArray(double[] array)
+{
+    Console.Write("[");
+
+    for (long i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+
+        if (i != array.Length - 1)
+            Console.Write(", ");
+    }
+
+    Console.Write("]");
+    Console.WriteLine();
+}
+double[] array = createArray();
+printArray(array);
+Sort(array);
+void Sort(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (max < array[i]) max = array[i];
+        if (min > array[i]) min = array[i];
+    }
+    Console.WriteLine("Максимальный элемент: " + max);
+    Console.WriteLine("Минимальный элемент: " + min);
+    double result = max - min;
+    Console.WriteLine("Разница между максимальным и минимальным элементом массива: " + result);
+}
+
